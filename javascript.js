b3eaccc9199ae772deaf77a08a2af282ac2be9e7
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2026-05-03 21:55:08 +0530

    added color

--- javascript.js
+++ javascript.js
@@ -4,4 +4,5 @@ btn.addEventListener("click", () => {
   const text = document.getElementById("text");
   const nameFromPrompt = prompt("Enter your name");
   text.textContent = nameFromPrompt;
+  text.style.color = "green"
 });
